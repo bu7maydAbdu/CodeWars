0a921e23f38f5d5538a9e6f5c0b48b24b7cdfc95
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-05-08 02:57:51 +0300

    fb

---
+++ fakeBinary.js
@@ -0,0 +1,18 @@
+function fakeBin(x) {
+    return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
+
+
+
+
+
+
+// return function fakeBin(x){
+//     x.split("").map(e=>{if(e < 5){
+//        return e = 0
+//       }else{
+        
+//   return  e = 1
+        
+//       }}).join("")
+//   }
