4d1ac9342400afad532fb5b1978ce5d97cdde28f
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-09-03 04:26:18 +0300

    reversed words

---
+++ reversedWords.js
@@ -0,0 +1,11 @@
+function reverseWords(str){
+    const splitted = str.split(" ")
+   const reversed =  splitted.reverse()
+   return reversed.join(" ")
+}
+
+   
+
+   // another solution in one line 
+
+    // return str.split(' ').reverse().join(' ')
