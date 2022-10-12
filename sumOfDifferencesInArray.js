02c2937db41d9ba81fbcbbe0380dd62ee9b1a025
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-10-12 19:39:11 +0300

    more solutions for sumOfDifferencesInArray

--- sumOfDifferencesInArray.js
+++ sumOfDifferencesInArray.js
@@ -6,3 +6,26 @@
 function sumOfDifferences(arr) {
     return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
 }
+
+
+
+//solution 2
+const sumOfDifferences = arr => arr
+  .sort((a, b) => b - a)
+  .map((a, i) => a - arr[i + 1] || 0)
+  .reduce((a, b) => a + b, 0);
+
+
+
+
+
+  //solution 3
+
+  function sumOfDifferences(arr) {
+    arr.sort((a, b) => a - b);
+    let b = 0;
+    for(let a = arr.length - 1; a > 0; a--) {
+      b += arr[a] - arr[a - 1];
+    }
+    return b;
+  }
