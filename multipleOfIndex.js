e4c6d033c2555b026cfd9a234f3e8c5e1d9e2089
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-10-12 23:37:55 +0300

    multiple of index optimum solution

--- multipleOfIndex.js
+++ multipleOfIndex.js
@@ -33,6 +33,15 @@ function multipleOfIndex(array) {
   }
 
 
+
+
+  // optimum solution
+
+  function multipleOfIndex(array) {
+    return array.filter((num, i) => num % i === 0);
+  }
+
+
   // tests : 
 
  console.log(multipleOfIndex([22, -6, 32, 82, 9, 25]), "[-6, 32, 25]") 
