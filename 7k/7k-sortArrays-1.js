f973e036348a3fea06178a101703a14a8fbc4f50
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-11-04 05:46:31 +0300

    another solution 7k sort arrays 1

--- 7k/7k-sortArrays-1.js
+++ 7k/7k-sortArrays-1.js
@@ -11,3 +11,13 @@
 const sortme = function( names ){
     return names.sort()
   }
+
+
+
+
+  // another solution:
+
+
+  sortme = function( names ){
+    return names.sort(function(a, b) { return a > b ? 1 : a < b ? -1 : 0 });
+  }
