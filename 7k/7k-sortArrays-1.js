1aab6209a78c048d3d7140f54f2c91f869075e43
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-11-04 05:44:27 +0300

    7k sort  arrays 1

---
+++ 7k/7k-sortArrays-1.js
@@ -0,0 +1,13 @@
+//case:
+
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+
+
+
+//solution:
+
+
+const sortme = function( names ){
+    return names.sort()
+  }
