aa579aeef4fdd40941a8245bf36d63e448a17498
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-11-12 08:32:31 +0300

    7k flatten

---
+++ 7k/7k-flatten.js
@@ -0,0 +1,19 @@
+//case:
+
+
+// Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+// flatten([1,2,3]) // => [1,2,3]
+// flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+// flatten([[[1,2,3]]]) // => [[1,2,3]]
+
+
+
+
+//solution:
+
+
+function flatten (array){
+    return array.forEach(e => ...e))
+   }
+   
