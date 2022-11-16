2f85d5ddc0afeb1184c03fe8e376707e759182d5
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-11-16 07:02:05 +0300

    7k number of occurences

---
+++ 7k/7k-numberOfOccurences.js
@@ -0,0 +1,22 @@
+//case:
+
+
+
+// Write a function that returns the number of occurrences of an element in an array.
+
+// Examples
+// var arr = [0, 1, 2, 2, 3];
+// arr.numberOfOccurrences(0) === 1;
+// arr.numberOfOccurrences(4) === 0;
+// arr.numberOfOccurrences(2) === 2;
+// arr.numberOfOccurrences("a") === 0;
+
+
+
+
+//solution:
+
+
+Array.prototype.numberOfOccurrences = function(search) {
+    return this.filter( function(num){ return search === num } ).length;
+  }
