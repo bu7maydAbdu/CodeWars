018fd2d95e4634feb4bbc12a16bdf6202abf5003
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-11-17 07:16:24 +0300

    another solution 6k sort the odd

--- 6k-sortTheOdd.js
+++ 6k-sortTheOdd.js
@@ -16,3 +16,33 @@ function sortArray(array) {
     const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
     return array.map((x) => x % 2 ? odd.shift() : x);
   }
+
+
+
+
+
+  // another solution:
+
+
+  function sortArray(array) {
+    let odds = [];
+    //loop, if it's odd, push to odds array
+    for (var i = 0; i < array.length; ++i) {
+      if (array[i]%2 !== 0) {
+        odds.push(array[i]);
+      }
+    }
+    //sort odds from smallest to largest
+    odds.sort(function(a,b){
+      return a-b;
+    });
+    
+    //loop through array, replace any odd values with sorted odd values
+    for (var j = 0; j < array.length; ++j) {
+      if (array[j]%2 !== 0) {
+        array[j] = odds.shift();
+      }
+    }
+    
+   return array;
+  }
