cf564ec3bc2d026d0b08d0b3ebfae63a6a951524
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-12-01 08:51:51 +0000

    Another solution  7k looking for benefectoe

--- 7k/7k-lookingForBenefector.js
+++ 7k/7k-lookingForBenefector.js
@@ -36,10 +36,18 @@
 //or return `-1` or ERROR or Nothing or None depending on the language.
 
 
+//solution :
+function newAvg(arr, newavg) {
+    if (arr.length === 0) return newavg;
+    const actualAvg = arr.reduce((a,c) => a+c, 0) / arr.length;
+    const amtToAdd = Math.ceil(arr.length * (newavg - actualAvg) + newavg); 
+    if (amtToAdd > 0) return amtToAdd; else throw ('Expected New Average is too low');
+}
 
 
 
-// solution:
+
+// alleged  solution:
 
 
 function newAvg(arr, newavg) {
