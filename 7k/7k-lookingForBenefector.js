9c254c1c9e0d85f104a32766ecb0c171f05bcbd1
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-12-01 08:47:51 +0000

    7k looking for benefector

--- 7k/7k-lookingForBenefector.js
+++ 7k/7k-lookingForBenefector.js
@@ -1,43 +1,57 @@
 //case:
 
- JavaScript
-Node v14.x
-VIM
-EMACS
-Instructions
-Output
-The accounts of the "Fat to Fit Club (FFC)" association are supervised by John as a volunteered accountant. The association is funded through financial donations from generous benefactors. John has a list of the first n donations: [14, 30, 5, 7, 9, 11, 15] He wants to know how much the next benefactor should give to the association so that the average of the first n + 1 donations should reach an average of 30. After doing the math he found 149. He thinks that he could have made a mistake.
+ 
+//The accounts of the "Fat to Fit Club (FFC)" association are supervised by John as a volunteered accountant. The association is funded through financial donations from generous benefactors. John has a list of the first n donations: [14, 30, 5, 7, 9, 11, 15] He wants to know how much the next benefactor should give to the association so that the average of the first n + 1 donations should reach an average of 30. After doing the math he found 149. He thinks that he could have made a mistake.
 
-if dons = [14, 30, 5, 7, 9, 11, 15] then new_avg(dons, 30) --> 149
+//if dons = [14, 30, 5, 7, 9, 11, 15] then new_avg(dons, 30) --> 149
 
-Could you help him?
+//Could you help him?
 
-Task
-The function new_avg(arr, navg) should return the expected donation (rounded up to the next integer) that will permit to reach the average navg.
+//Task
+//The function new_avg(arr, navg) should return the expected donation (rounded up to the next integer) that will permit to reach the average navg.
 
-Should the last donation be a non positive number (<= 0) John wants us:
+//Should the last donation be a non positive number (<= 0) John wants us:
 
-to return:
+//to return:
 
-Nothing in Haskell, Elm
-None in F#, Ocaml, Rust, Scala
--1 in C, D, Fortran, Nim, PowerShell, Go, Pascal, Prolog, Lua, Perl, Erlang
-or to throw an error (some examples for such a case):
+//Nothing in Haskell, Elm
+//None in F#, Ocaml, Rust, Scala
+//-1 in C, D, Fortran, Nim, PowerShell, Go, Pascal, Prolog, Lua, Perl, Erlang
+//or to throw an error (some examples for such a case):
 
-IllegalArgumentException() in Clojure, Java
-ArgumentException() in C#
-echo ERROR in Shell
-argument-error in Racket
-std::invalid_argument in C++
-ValueError in Python
-So, he will clearly see that his expectations are not great enough. In "Sample Tests" you can see what to return.
+//IllegalArgumentException() in Clojure, Java
+//ArgumentException() in C#
+//echo ERROR in Shell
+//argument-error in Racket
+//std::invalid_argument in C++
+//ValueError in Python
+//So, he will clearly see that his expectations are not great enough. In "Sample Tests" you can see what to return.
 
-Notes:
-all donations and navg are numbers (integers or floats), arr can be empty.
-See examples below and "Sample Tests" to see which return is to be done.
-new_avg([14, 30, 5, 7, 9, 11, 15], 92) should return 645
-new_avg([14, 30, 5, 7, 9, 11, 15], 2) 
-should raise an error (ValueError or invalid_argument or argument-error or DomainError or ... ) 
-or return `-1` or ERROR or Nothing or None depending on the language.
+//Notes:
+//all donations and navg are numbers (integers or floats), arr can be empty.
+//See examples below and "Sample Tests" to see which return is to be done.
+//new_avg([14, 30, 5, 7, 9, 11, 15], 92) should return 645
+//new_avg([14, 30, 5, 7, 9, 11, 15], 2) 
+//should raise an error (ValueError or invalid_argument or argument-error or DomainError or ... ) 
+//or return `-1` or ERROR or Nothing or None depending on the language.
+
+
+
+
+
+// solution:
+
+
+function newAvg(arr, newavg) {
+  let out = 0;
+  if ( arr.length !== 0 ){
+    let temp = newavg * (arr.length + 1) - arr.reduce((a, b) => a + b, 0)
+    out = Math.ceil(temp)
+  } else {
+    out = newavg
+  }
+  return out <= 0 ? error : out;
+}
+
 
   
