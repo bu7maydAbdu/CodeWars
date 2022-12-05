41bc2444b380d08923bb69856b8aa42c41baf077
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-12-05 06:41:09 +0300

    another solution 7k coding meetuo #4 Higher-Order_Functions Series - Find python developer

--- 7k/7k-codingMeetup4-Higher-Order-FunctionsSeries-FindTheFirstPythonDeveloper.js
+++ 7k/7k-codingMeetup4-Higher-Order-FunctionsSeries-FindTheFirstPythonDeveloper.js
@@ -45,4 +45,17 @@ function getFirstPython(list) {
     }
   
   }
+
+
+
+
+
+
+  // anothe solution:
+
+
+  function getFirstPython(list) {
+    const dev = list.find(x => x.language === "Python")
+    return dev ? `${dev.firstName}, ${dev.country}` : "There will be no Python developers"
+  }
   
