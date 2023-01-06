bcaba0032bc008447d999776f9028f860a5e6574
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-01-07 01:33:47 +0300

    the feast of many beasts

---
+++ grasshopper-personalizedMessage.js
@@ -0,0 +1,26 @@
+//case :
+
+
+
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+
+
+//solution :
+
+
+function greet (name, owner) {
+    if(name === owner){
+      return "Hello boss"
+    }else {
+      return "Hello guest"
+    }
+  }
+
+
+
+
+
+
+
+
