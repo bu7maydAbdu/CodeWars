79b4cc58b86963fe51d24242b277bae0043926e6
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-02-01 02:59:26 +0300

    7k greatest common divider

---
+++ 7k-greatestCommonDivider.js
@@ -0,0 +1,38 @@
+//case:
+
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+
+
+//solution:
+
+
+
+//recursive solution:
+
+function mygcd(x,y){
+    //your code here
+    if (!y) {
+      return x;
+    }
+  
+    return mygcd(y, x % y);
+  }
+
+  
+  // iterative // loop:
+
+  function mygcd(x,y){
+    for(let i = Math.min(x, y) ; i > 0 ; i--){
+ 
+     if(x % i === 0 && y % i === 0){
+       return i
+     }
+     
+   }
+ 
+ 
+ }
+ 
